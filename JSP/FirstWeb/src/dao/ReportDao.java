229668c10cb1f2dd8c098db502b532a5f730ad65
a4-01 <21400389@handong.edu>
2020-07-10 17:25:53 +0900

    DAO

---
+++ JSP/FirstWeb/src/dao/ReportDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Report;
+
+
+public class ReportDao {
+	private ReportDao() {
+	}
+	private static ReportDao dao = new ReportDao();
+	public static ReportDao getInstance() {
+		return dao;
+	}
+
+	public int insertReport(Connection conn, Report report) throws SQLException {
+		PreparedStatement pstmt = null;
+		int resultCnt = 0;
+		
+		String sql = "insert into report values (report_seq.nextVal, ?, ?, ?)";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, report.getSname());
+			pstmt.setString(2, report.getStn());
+			pstmt.setString(3, report.getReport());
+			
+			resultCnt = pstmt.executeUpdate();
+		} finally {
+			if(pstmt != null) {
+				pstmt.close();
+			}
+		}
+		
+		return resultCnt;
+	}
+}
