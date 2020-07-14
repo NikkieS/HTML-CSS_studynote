c7712aedca76f0b2f4cf80a817623be51ab3ba09
a4-01 <21400389@handong.edu>
2020-07-14 13:16:01 +0900

    OP MVC

---
+++ JSP/OpenProjectMVC/src/member/model/Member.java
@@ -0,0 +1,85 @@
+package member.model;
+
+import java.sql.Date;
+
+public class Member {
+	
+	private int idx;
+	private String uid;
+	private String upw;
+	private String uname;
+	private String uphoto;
+	private Date regdate;	// sql에서 불러오기 때문에 java.sql.Date로 import해준다
+	
+	public Member(int idx, String uid, String upw, String uname, String uphoto, Date regdate) {
+		this.idx = idx;
+		this.uid = uid;
+		this.upw = upw;
+		this.uname = uname;
+		this.uphoto = uphoto;
+		this.regdate = regdate;
+	}
+
+	public Member() {
+	}
+
+	public int getIdx() {
+		return idx;
+	}
+
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+
+	public String getUid() {
+		return uid;
+	}
+
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+
+	public String getUpw() {
+		return upw;
+	}
+
+	public void setUpw(String upw) {
+		this.upw = upw;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getUphoto() {
+		return uphoto;
+	}
+
+	public void setUphoto(String uphoto) {
+		this.uphoto = uphoto;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+	public java.util.Date getToDate(){	// ${member.todate}
+		// sql date type -> util date type 변환
+		return new java.util.Date(regdate.getTime());
+	}
+
+	@Override
+	public String toString() {
+		return "Member [idx=" + idx + ", uid=" + uid + ", upw=" + upw + ", uname=" + uname + ", uphoto=" + uphoto
+				+ ", regdate=" + regdate + "]";
+	}
+	
+}
--- JSP/OpenProjectMVC/src/member/service/MemberRegFormServiceImpl.java
+++ JSP/OpenProjectMVC/src/member/service/MemberRegFormServiceImpl.java
@@ -1,11 +1,11 @@
-package member;
+package member.service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import service.Service;
 
-public class MemberRegServiceImpl implements Service {
+public class MemberRegFormServiceImpl implements Service {
 
 	@Override
 	public String getViewPage(HttpServletRequest request, HttpServletResponse response) {
